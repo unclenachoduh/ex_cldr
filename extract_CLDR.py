6b319fdd43edf4e931a63d25789e3fc86c29407b
unclenachoduh <kekoariggin@gmail.com>
2018-02-16 17:23:34 -0800

    Clean script. Removed debugging ouput.

--- extract_CLDR.py
+++ extract_CLDR.py
@@ -1,25 +1,27 @@
 import urllib.request
 import re
+import os
 
 home = urllib.request.urlopen("http://bugs.icu-project.org/trac/browser/trunk/icu4c/source/data/lang?order=name")
 home_text = home.read().decode('utf-8', "strict")
-wout = open("home.html", "w+")
 home_lines = home_text.split("\n")
 langs = [] # list of language docs
 for line in home_lines:
     line = re.sub("\s+", " ", line)
     line = re.sub("^\s", "", line)
-    # print(line)
     parts = line.split(" ")
     if len(parts) > 2 and parts[0] == "<a" and parts[1] == "class=\"file\"":
         me = re.split("[><]", parts[4])
-        # print(me[1])
         langs.append(me[1])
 
+directory = "ftl_files/"
+if not os.path.exists(directory):
+    os.makedirs(directory)
+
 for lang in langs:
-    # print("\n\n\n\n" + lang + "\n\n\n\n")
+    # print(lang)
     new_file = re.sub(".txt", ".ftl", lang)
-    wout = open("ftl_files/" + new_file, "w+")
+    wout = open(directory + new_file, "w+")
     if lang[-4:] == ".txt":
         source = urllib.request.urlopen("http://icu-project.org/trac/browser/trunk/icu4c/source/data/lang/" + lang + "?format=txt")
         text = source.read().decode('utf-8', "strict")
@@ -28,12 +30,9 @@ for lang in langs:
         catch = False
         for line in text_lines:
             line = re.sub("\ufeff", "", line)
-            # print("BASIC", line)
             if bool(re.search("^//", line)) == False and bool(re.search("[(/**)(**/)]", line)) == False and line != '':
                 line = re.sub("\s+", " ", line)
                 line = re.sub("^\s", "", line)
-
-                # print("NOT COMMENT")
 
                 if "{" in line and "}" not in line:
                     interior += 1
@@ -43,14 +42,6 @@ for lang in langs:
                     interior -= 1
                     catch = False
                 elif len(re.findall("{", line)) < 2 and len(re.findall("}", line)) < 2:
-                    # print(line)
                     if catch == True:
                         parts = re.split("[\{\}\"]", line)
-                        # print(parts)
-                        # print("PAIR:", parts[0], "=", parts[2])
                         wout.write(parts[0] + " = " + parts[2] + "\n")
-
-
-
-# wout = open("es.txt", "w+")
-# wout.write(text)
---
+++ readme.md
@@ -0,0 +1,13 @@
+# Script for bug 1433694
+
+*[Link to bug](https://bugzilla.mozilla.org/show_bug.cgi?id=1433694)*
+
+## Description
+
+This script downloads all of the CLDR files available from the [ICU Project website]( http://bugs.icu-project.org/trac/browser/trunk/icu4c/source/data/lang?order=name),  extracts the language ID data, and converts it to Fluent format.
+
+## Running the script
+
+This script is meant to be ran in the terminal with Python 3.
+
+`python3 extract_CLDR.py`
